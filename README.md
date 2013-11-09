c5f7d17d684e146a7053a0466b3e605a03beb156
Niall Paterson <npaters@tcd.ie>
2013-11-09 14:02:00 +0000

    Update phosphorus.js and README.md

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 Phosphorus
 ========
 
-Phosphorus is a key store system based on redis that has a lot more under the hood. It's lightning fast because it intelligently caches everything in localStorage and only actually hits our server once per session. The first request takes around 100ms, and  everyone after that takes a millisecond *even if the data changes on the server*. I'll say that again - **Less than a millisecond even if the data changes on the server**
-
-It'll also allow you to listen out for when the data changes.
+Phosphorus is a client side key-value store that uses redis to sync data with every other client. This allows it to be used to sync real time data across clients and have a semi-permanent data store backing it up.
 
 Use Cases
 ---------
 
-Phosphorus is ideal in any scenerio where you have simple non sensitive that needs to be shared over different computers, but where you still need a central store. A simple example would be the following:
+Phosphorus is ideal in any scenerio where you have simple non sensitive that needs to be shared over different clients, but where you still need a central store. A simple example would be the following:
 
-Say you wanted to implement real time updating of the number of views a video gets on youtube. Suppose we've a function called `updateViewCount()` which takes a number and updates the view count on the screen to that. Let's use `http://www.youtube.com/watch?v=D-0DUPowEw0` as a sample video. Let's use that identifier `D-0DUPowEw0` as a unique identifier. Using Phosphorus, it'd be as simple as this:
+Say you wanted to implement real time updating of the number of views a video gets on Youtube. Suppose we've a function called `updateViewCount()` which takes a number and updates the view count on the screen to that. Let's use `http://www.youtube.com/watch?v=D-0DUPowEw0` as a sample video. Let's use that identifier `D-0DUPowEw0` as a unique identifier. Using Phosphorus, it'd be as simple as this:
 
     var key = 'D-0DUPowEw0:viewCount' //can be anything
     Phosphorus.get(key, function (count) {
@@ -31,10 +29,6 @@ Now say there was somewhere else on the page where you needed the count. All you
     })
     
 This will be instantious because it'll come from localstorage, but don't worry, as the value changes, localstorage will update, so you'll have the latest value always. Again, use Phosphorus.listen to react to changes.
-    
-    
-    
-
 
 Installation
 -------
@@ -43,16 +37,7 @@ Phosphorus relies on socket.io and zepto/jquery. phosphorus.js and phosphorus.mi
 
      <script src="http://cdnjs.cloudflare.com/ajax/libs/zepto/1.0/zepto.min.js"></script>
      <script src="http://cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.6/socket.io.min.js"></script>
-     <script src="phosphorus.min.js"></script>
-
-
-Then simply set a token:
-
-    <script>
-        Phosphorus.token = '1'
-    </script>
-    
-Try to make sure your token is unique, it can be alphanumeric and can have underscores in it. If it's not unique then you risk writing over someone else's data, or vice versa. See todo.
+     <script src="/src/phosphorus.min.js"></script>
 
 Features / Usage
 ----------------
@@ -100,19 +85,6 @@ Clear's local caching, automatically run when page closes, but can be used at an
 Stop automatically listening changing the value of a key. This can also take an optional callback as the second argument.
 
 
-----------------------
-
-
-**Sidenote -** if you don't want to type out `Phosphorus` the whole time, you can use `Fos4us` to save time, just don't mix the two!
-
 License
 -------
 Released under the MIT License, fork away, pull requests are more than welcome.
-
-TO-DO
------
-
-1. Angular/Ember integration
-2. Auth. Phosphorus is completely insecure right now.
-3. Token generation/checking system
-4. Upgrade servers, seperate redis instance. Right now, phosphorus runs off a tiny $5 a month instance, improving this should speed it up massively.
--- client/phosphorus.js
+++ client/phosphorus.js
@@ -1,116 +1,96 @@
-Phosphorus = {}
-Phosphorus.baseUrl = 'http://146.185.144.4'
+Phosphorus = {};
+Phosphorus.baseUrl = 'http://127.0.0.1';
 Phosphorus.socket = io.connect(Phosphorus.baseUrl);
 
 // events
 
 Phosphorus.get = function (key, callback) {
-  if (!Phosphorus.token) {
-    return callback('token not set')
+  if (Phosphorus.watched().contains(key.toString())) {
+    return callback(JSON.parse(localStorage[key.toString()]));
   }
-  token_and_key = Phosphorus.token.toString() + ':' + key.toString()
-  if (Phosphorus.watched().contains(token_and_key)) {
-    return callback(JSON.parse(localStorage[token_and_key]))
-  }
-  var url = '/get'
-  var data = {
-    token: Phosphorus.token,
-    key: key
-  }
+  var url = '/get';
+  var data = {key: key};
   ajax(url, data, function (val) {
-    localStorage[token_and_key] = JSON.stringify(val)
-    callback(val)
-  })
-  Phosphorus.subscribe(key)
+    localStorage[key.toString()] = JSON.stringify(val);
+    callback(val);
+  });
+  Phosphorus.subscribe(key);
 }
 
 Phosphorus.set = function (key, value) {
-  //subscribe on set?
-  if (!Phosphorus.token) {
-    return console.log('token not set')
-  }
-  var url = '/set'
+  var url = '/set';
   var data = {
-    token: Phosphorus.token,
     key: key,
     value: value
-  }
-  ajax(url, data)
-  Phosphorus.subscribe(key)
-  return value
+  };
+  ajax(url, data);
+  Phosphorus.subscribe(key);
+  return value;
 }
 
 Phosphorus.fetch = function (key, value, callback) {
-  if (!Phosphorus.token) {
-    return callback('token not set')
-  }
   Phosphorus.get(key, function (val) {
     if (!val) {
-      Phosphorus.set(key, value)
-      return callback(value)
+      Phosphorus.set(key, value);
+      return callback(value);
     }
     else {
-      return callback(val)
+      return callback(val);
     }
-  })
+  });
 }
 
 Phosphorus.listen = function (key, fn) {
   document.addEventListener('phosphorus-' + key,function (e) {
-    fn.apply(null,[e.detail])
-  })
+    fn.apply(null,[e.detail]);
+  });
 }
 
 Phosphorus.subscribe = function (key) {
-  Phosphorus.addToWatched(key)
-  token_and_key = Phosphorus.token.toString() + ':' + key.toString()
-  if (!Phosphorus.socket.$events || !Phosphorus.socket.$events[token_and_key]) { // check if socket on event is already set
-    Phosphorus.socket.on(token_and_key, function (val) {
-      console.log('socket emission received!')
+  Phosphorus.addToWatched(key);
+  if (!Phosphorus.socket.$events || !Phosphorus.socket.$events[key.toString()]) { // check if socket on event is already set
+    Phosphorus.socket.on(key.toString(), function (val) {
       var evt = document.createEvent("Event");
       evt.initEvent("phosphorus-"+key.toString(), true, false);
-      evt.detail = val
-      document.dispatchEvent(evt)
-      localStorage[token_and_key] = JSON.stringify(val)
-    })
+      evt.detail = val;
+      document.dispatchEvent(evt);
+      localStorage[key.toString()] = JSON.stringify(val);
+    });
   }
 }
 
 Phosphorus.unsubscribe = function (key, callback) {
-  token_and_key = Phosphorus.token.toString() + ':' + key.toString()
-  Phosphorus.socket.removeAllListeners(token_and_key)
-  if (callback)
-    callback()
+  Phosphorus.socket.removeAllListeners(key.toString());
+  if (callback) {
+    callback();
+  }
 }
 
 Phosphorus.restart = function () {
   // delete localstorage for each in localStorage['watched']
   if (localStorage['watched']) {
-    watched = Phosphorus.watched()
+    watched = Phosphorus.watched();
     for(var i = 0; i < watched.length; i++) {
-      localStorage.removeItem(watched[i])
+      localStorage.removeItem(watched[i]);
     }
-    localStorage.removeItem('watched')
+    localStorage.removeItem('watched');
   }
-  return '' // for onbeforeunload
+  return ''; // for onbeforeunload
 }
 
 // helper functions
 
 Phosphorus.addToWatched = function (key) {
-  watched = Phosphorus.watched()
-  token_and_key = Phosphorus.token.toString() + ':' + key.toString()
-  watched.push(token_and_key)
-  localStorage['watched'] = JSON.stringify(watched)
+  watched = Phosphorus.watched();
+  watched.push(key.toString());
+  localStorage['watched'] = JSON.stringify(watched);
 }
-
-
 
 Phosphorus.watched = function () {
   if (!localStorage['watched']) {
-    return []
+    return [];
   }
-  return JSON.parse(localStorage['watched'])
+  return JSON.parse(localStorage['watched']);
 }
 
 // other helper functions
@@ -128,11 +108,10 @@ Array.prototype.contains = function(obj) {
 var ajax = function (url, data, callback) {
   $.post(Phosphorus.baseUrl + url, data, function(res) {
     if (callback) {
-      callback(res)
+      callback(res);
     }
-  })
+  });
 }
 
-window.onbeforeunload = Phosphorus.restart()
-window.Phosphorus = Phosphorus
-window.Fos4us = Phosphorus 
+window.onbeforeunload = Phosphorus.restart();
+window.Phosphorus = Phosphorus;
